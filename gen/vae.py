23dd54031bf61d26660cb619c7182376f587e155
eschmidt42 <11818904+eschmidt42@users.noreply.github.com>
2021-10-21 20:52:39 +0200

    adjusted VAE class to the order of the methods is the same as the AE class. adopted the passing of encoding and decoding models and usage of ae.get_encoder_model and ae.get_decoder_model as defaults.

--- gen/vae.py
+++ gen/vae.py
@@ -1,4 +1,4 @@
-from gen import utils
+from gen import utils, ae
 import torch
 from torch import nn, Tensor
 import torch.nn.functional as F
@@ -11,45 +11,37 @@ class VAE(nn.Module):
     with some random alterations for the layers.
     """
 
-    def __init__(self, n_in: int, n_h: int = 50, n_z: int = 10, p: float = 0.01):
+    def __init__(
+        self,
+        n_in: int,
+        n_h: int = 50,
+        n_z: int = 10,
+        p: float = 0.01,
+        mean_encoder_model: nn.Module = None,
+        logvar_encoder_model: nn.Module = None,
+        decoder_model: nn.Module = None,
+    ):
         super().__init__()
         utils.logger.info(f"n_in {n_in}, n_h {n_h}, n_z {n_z}")
-        self.in_layer = nn.Linear(n_in, n_h)
 
-        self.z_mean = nn.Sequential(
-            self.in_layer,
-            nn.BatchNorm1d(n_h),
-            nn.Dropout(p),
-            nn.ReLU(),
-            nn.Linear(n_h, n_z),
-        )  #  self.mean_layer
-        self.z_logvar = nn.Sequential(
-            self.in_layer,
-            nn.BatchNorm1d(n_h),
-            nn.Dropout(p),
-            nn.ReLU(),
-            nn.Linear(n_h, n_z),
-        )  # self.var_layer
-
-        self.decoder = nn.Sequential(
-            nn.Linear(n_z, n_h),
-            nn.BatchNorm1d(n_h),
-            nn.Dropout(p),
-            nn.ReLU(),
-            nn.Linear(n_h, n_in),
-            nn.BatchNorm1d(n_in),
+        self.z_mean = (
+            ae.get_encoder_model(n_in, n_h, p, n_z)
+            if mean_encoder_model is None
+            else mean_encoder_model
         )
-
-    def get_distribution_statistics(self, x):
-        return self.z_mean(x), self.z_logvar(x)
+        self.z_logvar = (
+            ae.get_encoder_model(n_in, n_h, p, n_z)
+            if logvar_encoder_model is None
+            else logvar_encoder_model
+        )
+        self.decoder = (
+            ae.get_decoder_model(n_in, n_h, p, n_z)
+            if decoder_model is None
+            else decoder_model
+        )
 
     def decode(self, z):
         return self.decoder(z)
-
-    def reparameterize(self, mu: Tensor, logvar: Tensor) -> Tensor:
-        std = torch.exp(0.5 * logvar)
-        eps = torch.randn_like(std)
-        return eps * std + mu
 
     def encode(self, x_cat, x_cont):
         μ, logvar = self.get_distribution_statistics(x_cont)
@@ -71,14 +63,22 @@ class VAE(nn.Module):
     ) -> Tensor:
         return self.forward(x_cat, x_cont)[0]
 
+    def get_distribution_statistics(self, x):
+        return self.z_mean(x), self.z_logvar(x)
+
+    def reparameterize(self, mu: Tensor, logvar: Tensor) -> Tensor:
+        std = torch.exp(0.5 * logvar)
+        eps = torch.randn_like(std)
+        return eps * std + mu
+
 
 class VAE_Loss:
     """
     https://arxiv.org/pdf/1312.6114.pdf -> eq. 10
     """
 
-    def __init__(self, bs):
-        self.bs = bs
+    def __init__(self, kld_weight: float = 1.0):
+        self.kld_weight = kld_weight  # importance of the Kullback-Leibler Loss - may be the batchsize or something else
 
     def loss(self, reconstructions, originals, mu, logvar) -> dict:
         reconstruction_loss = F.mse_loss(reconstructions, originals)
@@ -86,12 +86,13 @@ class VAE_Loss:
         kld_loss = -torch.mean(
             0.5 * torch.sum(1 + logvar - mu ** 2 - logvar.exp() ** 2, dim=1), dim=0
         )
-        kld_weight = self.bs
-        loss = reconstruction_loss + kld_weight * kld_loss
+
+        loss = reconstruction_loss + self.kld_weight * kld_loss
         return {
             "loss": loss,
             "Reconstruction_Loss": reconstruction_loss,
-            "KLD": -kld_loss,
+            "KLD": kld_loss,
+            "kld_weight": self.kld_weight,
         }
 
     def __call__(self, X, y):
